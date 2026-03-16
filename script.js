727e08c2344486b2cba3c13930782d4cce9dbfbb
Cintia Souza <cintiacpereirapereiraesouza@gmail.com>
2026-03-16 11:32:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 function login() {
     const user = document.getElementById('usuario').value;
     const pass = document.getElementById('senha').value;
-    const mensagem = document.getElementById('mensagem');
+    const message = document.getElementById('mensagem');
 
-    const userCorreto = "admin";
-    const senhaCorreta = "1234";
+    const usuarios = [
+        { usuario: "admin", senha: "1234" },
+        { usuario: "joao", senha: "5678" },
+        { usuario: "alex", senha:"senha123"},
+        { usuario: "cintia", senha: "abcd" },
+        { usuario: "eduardo", senha: "0000" }
+    ];
 
-    if (user === userCorreto && pass === senhaCorreta) {
-        window.location.href = "segunda.html"
-    } else {
-        mensagem.textContent = "Usuário ou senha incorretos!"
+    let loginValido = false;
+
+    for (let i = 0; i < usuarios.length; i++) {
+
+        if (user === usuarios[i].usuario && pass === usuarios[i].senha) {
+            loginValido = true;
+            break;
+        }
+
     }
 
+    if (loginValido) {
+        window.location.href = "segunda.html";
+    } else {
+        message.textContent = "Usuário ou senha incorretos!";
+    }
 }
